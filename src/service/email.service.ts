7f6419591380925463e90ba5f4285f185d8d5010
lilongji <woshililongji@163.com>
2020-06-09 17:00:30 +0800

    暂时屏蔽掉邮件发送

--- src/service/email.service.ts
+++ src/service/email.service.ts
@@ -19,16 +19,17 @@ export class EmailService {
     try {
       if (!this.transporter) throw new Error('未初始化邮箱');
       console.log(444444444, this.transporter);
-      const contentUrl =  this.envService.getConfig().contentUrl + content.demandId;
-      this.transporter.sendMail({
-        from: `"需求管理平台邮件" <${this.emailConfig.auth.user}>`,
-        to: toUserEmail,
-        subject: '小伙子，你被安排了',
-        html: `<p>${content.fromUserName} 给你安排了一个需求，请打开<a href="${contentUrl}">${contentUrl}</a></p>`,
-      }, (err, info) => {
-        console.log(555555555, err);
-        console.log(444444444, info);
-      });
+      // TODO 暂时不发
+      // const contentUrl =  this.envService.getConfig().contentUrl + content.demandId;
+      // this.transporter.sendMail({
+      //   from: `"需求管理平台邮件" <${this.emailConfig.auth.user}>`,
+      //   to: toUserEmail,
+      //   subject: '小伙子，你被安排了',
+      //   html: `<p>${content.fromUserName} 给你安排了一个需求，请打开<a href="${contentUrl}">${contentUrl}</a></p>`,
+      // }, (err, info) => {
+      //   console.log(555555555, err);
+      //   console.log(444444444, info);
+      // });
     } catch (error) {
       console.error('send emila error:', error);
     }
